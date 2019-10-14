ce9e6bbf48c1f391746d2bbaf2642af04d74ca61
Charles Xavier <jetweedy@yahoo.com>
2019-10-14 19:06:58 -0400

    M

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log("Listening on ${ PORT }"));
+  .listen(PORT, () => console.log("Listening on " + PORT));
 
 const wss = new SocketServer({ server });
 
@@ -17,10 +17,20 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
+wss.on('connection', (ws) => {
+  ws.on('message', message => {
+    ws.send(message+"+1="+(parseInt(message)+1));
+  })
+  ws.send('Welcome! You\'re connected.')
+})
 
 //// This is happening on the back end and pushing up!
+/*
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
+*/
+
+
