ced4a9b059f49a0de828919a4a3eb4aa00b696b5
Jon Tweedy <jetweedy@gmail.com>
2021-05-18 13:10:58 -0400

    changed to an anonymous websocket-only chat with usernames and rooms but no DB persistence

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
+
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
@@ -16,16 +18,28 @@ const wss = new SocketServer({ server });
 let counter = 0;
 
 wss.on('connection', (ws) => {
+//	if (typeof ws._data == "undefined") {
+	ws._data = {
+		uuid: uuidv4()
+	}
+//	}
 	ws.on('close', () => console.log('Client disconnected'));
-});
-wss.on('connection', (ws) => {
 	ws.on('message', message => {
 		let mdata = JSON.parse(message);
-		if (mdata.action=="increment") {
-            var m = {action:"counter", content:++counter};            
-			ws.send(JSON.stringify(m));
+		if (mdata.action=="chatmsg") {
+		    wss.clients.forEach((ws) => {
+				if (ws._data.room == mdata.room) {
+			        let m = {action:"chatmsg", content: {
+						message: mdata.message, username: mdata.username, timestamp: (new Date()).toTimeString()
+					}};
+					console.log(m);
+			        ws.send(JSON.stringify(m));					
+				}
+		    });
 		}
 		if (mdata.action=="init") {
+			ws._data.username = mdata.username;
+			ws._data.room = mdata.room;
             var m = {action:"welcome", content:"Welcome, " + mdata.username + "!"};
 			ws.send(JSON.stringify(m));
 		}
@@ -33,10 +47,12 @@ wss.on('connection', (ws) => {
 })
 
 //// This is happening on the back end and pushing up!
+/*
 setInterval(() => {
     wss.clients.forEach((ws) => {
+    	console.log(ws._data);
         let m = {action:"log", content:(new Date()).toTimeString()};
         ws.send(JSON.stringify(m));
     });
 }, 1000);
-
+*/
