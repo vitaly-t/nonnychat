b57e14210e56d1abf82f2a31714e1e465e1b8cdc
Jon Tweedy <jetweedy@gmail.com>
2021-05-19 14:48:51 -0400

    M

--- public/nonnychat.js
+++ public/nonnychat.js
@@ -1,24 +1,31 @@
 
 
-
+/*
+To make things simple, we're going to create a WSHOST variable 
+that contains a websockets URL based on the URL of our web address, 
+which we know will be "/" when we're on our index page.
+*/
 var WSHOST = location.origin.replace(/^http/, 'ws');
 
+//// Here I'm just initliaizing a number of global variables that will get set later
 var username = false;
 var room = false;
+var sock = false;
 var chatlog;
 var chatmsg;
 var inputroom;
 var inputusername;
 var btnSendMsg;
 var btnPressEnterToSend;
-var sock = false;
-
-
 var btni;
 var instructions;
 var btnJoin;
 
-
+/*
+This logout function will close up the websocket (if still open), 
+set a bunch of relevant variables back to false or whatever, 
+and show and hide a few divs where appropriate to appear logged out
+*/
 function logout(opts) {
     if (typeof opts == "undefined") {
         opts = {};
@@ -41,35 +48,54 @@ function logout(opts) {
     chatmsg.removeEventListener("keyup", keyupChatMessage);
 }
 
-
+/*
+This is the function that runs if you click the Send button.
+It also gets called if the checkbox is checked and you hit Enter.
+(You'll see that logic forther down in keyupChatMessage.)
+*/
 function clickSendMessage() {
+    //// Trim the value entered.
     chatmsg.value = chatmsg.value.trim();
+    //// If it's not blank, then...
     if (chatmsg.value!="") {
+        //// Construct some message data:
         msgdata = {
             action:"chatmsg",
             message: chatmsg.value,
             room: room, 
             username: username
         };
+        //// Clear out the chat message input
         chatmsg.value = "";
+        //// Re-focus on that input (in case you clicked the button and left it)
         chatmsg.focus();
+        //// Send the message through the socket (build further below)
         sock.send(JSON.stringify(msgdata));
     }
 };
 
 function keyupChatMessage(e) {
+    //// Check if the 'press enter to send' checkbox is checked
+    //// AND if the key just pressed was Enter (code 13)
     if (cbPressEnterToSend.checked && e.keyCode==13) {
+        //// If so, run that clickSendMessage function from above
         clickSendMessage();
     }
 }
 
 
+//// This just formats each message text a little/
+//// Right now all it does is turn URLs into clickable links
 function getDisplayFormat(x) {
     var up = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
     x = x.replace(up, "<a target='_blank' href='$1'>$1</a>");
     return x;
 }
 
+
+/*
+This is the big kahuna...
+*/
 function joingame(username,room) {
 
     if (username!="" && room !="") {
@@ -133,31 +159,47 @@ function joingame(username,room) {
 }
 
 
+/*
+Once our window loads, we'll want to initialize a few things
+like references to various DOM elements in the page that
+we'll reference multiple times throughout our code.
+*/
 window.onload = function() {
 
     btni = document.querySelector("#btnToggleInstructions");
     instructions = document.querySelector("#instructions");
     btnJoin = document.querySelector("#joingame");
 
-
+    //// Add an "_open" boolean flag to our instructions div
+    instructions._open = false;
+    //// Add a click listener to our instructions toggler
     btni.addEventListener("click", function() {
         if (!instructions._open) {
+            //// If that boolean flag is false, then
+            //// set it to true
             instructions._open = true;
+            //// Show the instructions
             instructions.style.display = "block";
+            //// Change the innerHTML to what looks like an [X] to close it
             btni.innerHTML = "[X]";
         } else {
+            //// Otherwise do the opposite...
+            //// Set the flag to false
             instructions._open = false;
+            //// HIDE the instructions
             instructions.style.display = "none";
+            //// Replace that [X] with the words again
             btni.innerHTML = "Info / Instructions";
         }
     });
 
-    instructions._open = false;
+    //// Here we're adding a click listener to our Join button to 
+    //// initialize a chat using the joingame function
     btnJoin.addEventListener("click", () => {
-        var un = document.querySelector("#username");
-        username = un.value;
-        var rm = document.querySelector("#room");
-        room = rm.value;
+        //// Grab the username and room values that the entered
+        username = document.querySelector("#username").value;
+        room = rdocument.querySelector("#room").value;
+        //// Run the joingame function using those two values
         joingame(username, room);
     });
 }
--- server.js
+++ server.js
@@ -1,30 +1,53 @@
-'use strict';
 
-const { v4: uuidv4 } = require('uuid');
+//// Initiatlize an NPM project and Require a few more libraries that will require installation:
+/*
+npm init -y
+npm install dotenv
+npm install express
+npm install ws
+*/
 
-const express = require('express');
-const SocketServer = require('ws').Server;
+
+//// Require the (native) path library for generating paths to files
 const path = require('path');
 
+
+//// This line uses the dotenv library to make vars in a .env available (if present)
+//// For example, on Heroku, process.env.PORT will be set in the environment for you
+//// This line lets you access that value by referencing it that way
+require('dotenv').config()
+//// Now we'll look for that value, or set a default to 3000 locally if not found
+//// (So your app will be hosted at http://localhost:3000 when run locally)
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+
+//// Express is a library that provides some convenient routing syntax.
+//// Routing basically tells your server how to handle specific URL requests
+//// 	and with various parameters included (like ?a=1&b=yes, stuff like that)
+const express = require('express');
+//// Create a reference to a socket server created by leveraging the 'ws' websockets library
+//// This socket server will listen for browser connections and allow you to broadcast messages to them
+const socks = require('ws').Server;
 
 
-
+//// Now create and start up a server instance with express, with a few specifications:
 const server = express()
+	//// Specify a 'get' route for the homepage:
 	.get('/', function(req, res){
+		//// We'll just be serving up the index.html that's in the 'public' folder
 		res.sendFile(path.join(__dirname, 'public/index.html'));
 	})
+	//// We'll also use the /public directory as a static folder
+	//// Anything that goes to /public/whatever. will just serve up whatever.file
+	//// This is useful for using external JS and CSS in our index.html file, for example
 	.use('/public', express.static('public'))
+	//// And finally, let's listen on our PORT so we can visit this app in a browser
 	.listen(PORT, () => console.log("Listening on PORT " + PORT))
 	;
 
 
-const wss = new SocketServer({ server });
+const wss = new socks({ server });
 wss.on('connection', (ws) => {
-	ws._data = {
-		uuid: uuidv4()
-	}
+	ws._data = {}
 	ws.on('close', () => console.log('Client disconnected'));
 	ws.on('message', message => {
 		let mdata = JSON.parse(message);
